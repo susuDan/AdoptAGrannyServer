27fb4e7bfc13f01667208fe508a4beed41592098
susuDan <dancheung13@gmail.com>
2015-02-07 23:31:09 -0800

    Latest changes

--- cloud/app.js
+++ cloud/app.js
@@ -8,21 +8,13 @@ app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
 
-app.post('/requestahelper', function(req, res) {
-	
-	
-	var grannyEmail = req.body.Sender; // TODO: get granny email address
+app.post('/requestahelper', function(req, res) 
+{
+	var grannyEmail = req.body.Sender; 
 
-	// TODO: parse from body
 	var taskMessage=req.body['Text-part'];
 	var taskSubject=req.body.Subject;
-	console.log('body');
 	console.log(req.body);
 	
 	Parse.Cloud.httpRequest(
@@ -50,10 +42,7 @@ app.post('/requestahelper', function(req, res) {
 				console.log('no match found');
 			}
 			
-			var taskDurationHour = 1;
-			var taskDurationMin = 30;
-			var taskStartTimeHour = 12;
-			var taskStartTimeMin = 30;
+			var taskStartTime = "12:30 PM";
 
 			var Granny = Parse.Object.extend("Granny");
 			var query = new Parse.Query(Granny);
@@ -63,23 +52,22 @@ app.post('/requestahelper', function(req, res) {
 			{
 				 success: function(results) {
 					var granny = results[0];
-					var Task = Parse.Object.extend("Task");
+					var Task = Parse.Object.extend("Request");
 					var task = new Task();
 
 					task.save( 
 					{ 
-						category: taskCategory,
-						durationHour: taskDurationHour,
-						durationMin: taskDurationMin,
-						distance: 5.2,
+						requestName: taskCategory,
+						distance: "5.2",
 						grannyid: granny.id,
+						grannieName: granny.get("firstname") + ' ' + granny.get("lastname"),
+						img: "empty",
 						status: "Active",
-						startTimeHour: taskStartTimeHour,
-						startTimeMin: taskStartTimeMin,
+						startTime: taskStartTime,
 						message: taskMessage,
 						subject: taskSubject,
 						volunteerid: granny.id
-						// inline more granny information
+						
 					}).then
 					(
 						function(test) 
@@ -107,18 +95,24 @@ app.post('/requestahelper', function(req, res) {
 	});
 });
 
-
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
+Parse.Cloud.afterSave("Request", function(request) {
+	console.log("on save Request");
+	if(request.object.get("status") == "Accepted")
+	{
+		query = new Parse.Query("Volunteer");
+		query.get(request.object.get("volunteerid"), 
+		{
+			success: function(volunteer) 
+			{
+				console.log("Found volunteer");
+			},
+			error: function(error) 
+			{
+				console.error("Got an error ");
+			}
+		});
+	}
+});
 
 // Attach the Express app to Cloud Code.
 app.listen();
